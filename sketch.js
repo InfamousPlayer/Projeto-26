064701d49d0245120b3b9b60366db2ed46124589
InfamousPlayer <93223147+InfamousPlayer@users.noreply.github.com>
2022-01-24 19:15:08 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+Pave Simples de Chocolate
+
+    Ingredientes()
+
+        - 1 lata de creme de leite
+        - 1 lata de leite (a mesma medida da lata de creme de leite)
+        - 2 latas de leite condensado
+        - 2 gemas
+        - 2 colheres de maisena
+        - 3 colheres de chocolate em po ou achocolatado
+        - biscoito maisena
+        - leite para molhar os biscoitos 
+        - raspas de chocolate (opicional)
+        - manteiga sem sal ou margarina, so para dar o ponto
+    
+    
+Preparo()
+
+    1- Primeiro, pegue a lata de leite condensado, gemas, a maisena e o leite.
+    2- Coloque no fogo e deixe ate que fique um creme, reserve.
+    3- Coloque a segunda lata de leite condensado com um pouco de margarina e o chocolate.
+    4- Deixe no fogo ate ficar um creme tipo brigadeiro.
+    5- Desligue o fogo e junte com o creme de leite.
+
+
+Montagem()
+
+    1- Coloque primeiro o creme branco.
+    2- Em seguida, mergulhe os biscoitos no leite e coloque sobre o creme branco.
+    3- Depois, coloque o creme preto e acrescente raspas de chocolate sobre ele.
+    4- Deixe no congelador durante 2 horas.
